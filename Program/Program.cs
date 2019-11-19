daecf616ac2c867b304fd8c880a1585311b3c3ae
KeeolinNaicker <57951690+KeeolinNaicker@users.noreply.github.com>
2019-11-19 16:14:14 +0200

    Add files via upload

---
+++ Program/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Program
+{
+    class Program
+    {
+         
+        static void Main(string[] args)
+        {
+            var Basket = new Basket();
+
+            var Tshirt = new TShirt();
+            Tshirt.name = " DC Comics";
+            Tshirt.size = "L";
+            Basket.shirts.Add(Tshirt);
+
+            var golfer = new Golfer();
+            golfer.name = " Marvel";
+            golfer.size = "S";
+            Basket.shirts.Add(golfer);
+
+            var Formal = new FormalPants();
+            Formal.name = "Dark Horse";
+            Formal.size = "M";
+            Basket.pants.Add(Formal);
+
+
+            var jeans = new Jeans();
+            jeans.name = "Image Comics";
+            Formal.size = "L";
+            Basket.pants.Add(Formal);
+
+
+            Console.WriteLine($"The total is R{Basket.getTotal()}");
+            //Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
